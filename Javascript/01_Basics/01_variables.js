5f1ae30a06083488ac84cb194b37b17be28d43a9
Anshumaan <anshumaan.1611@gmail.com>
2024-09-22 12:05:44 +0530

    Add files via upload

---
+++ Javascript/01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountID=12345
+let accountEMAIL="anshumaan.1611@gmail.com"
+var accountPASSWORD="12345"
+accountCity="Jaipur"
+
+// CONST should not be changed
+
+
+accountEMAIL="mishraraghav1611@gmail.com"
+
+accountCity="Kanpur"
+
+
+accountPASSWORD="202o09i"
+console.log(accountID)
+
+console.table([accountID,accountEMAIL,accountPASSWORD,accountCity])
+
+// Prefer not to use var because of block scope and functional scope
+
+
+//we can define a variable without using var or let but we should never do so
---
+++ Javascript/01_Basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";  //use all js code as newer version this is automaticaaly added in the code but should be mentioned for other person to know
+
+
+//alert is used diffrently in node 
+
+//code readability is very important
+
+//tc39 and mdn documentation are also important
+
+let name ="Anshumaan"
+
+let age=19
+
+let isLoggedin=false
+
+
+
+/*number => 2 to the power53
+bigInt
+string=>""
+boolean=>true or false
+null=> standalone value
+undefined=> value is not assigned yet
+symbol=>used in react  for uniqueness
+object*/
+
+console.log(typeof name)
+
+
+console.log(typeof undefined)//undefined is a data type 
+console.log(typeof null)//object
+
